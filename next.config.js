cc2776b164dec8214842ea00cf026c921ee3a634
Pablo Jonatan <this.pablo@gmail.com>
2022-09-17 11:54:19 -0300

    chore: remove dotenv

--- next.config.js
+++ next.config.js
@@ -1,16 +1,9 @@
 /** @type {import('next').NextConfig} */
 const webpack = require('webpack')
 
-const { parsed: env } = require('dotenv').config({
-})
-
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  webpack(config) {
-    config.plugins.push(new webpack.EnvironmentPlugin(env))
-    return config
-  }
 }
 
 module.exports = nextConfig
